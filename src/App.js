727f8e0283e1c79c83a8ca52906e76bcb9b7a1e8
Ahsan-cod <66218657+Ahsan-cod@users.noreply.github.com>
2020-07-20 23:19:13 +0500

    check for auto deploy

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+<div> 
+<h1>Ahsan Ahmed
+</h1>
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Kasy hai ap sab
-      </h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code></code> and HI.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-        </a>
-      </header>
-      <footer>COPYRIGHT 2020 DESIGN THIS WEBSITE BY:AHSAN AHMED</footer>
-    </div>
-  );
-}
+</div>
 
 export default App;
